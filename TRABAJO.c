e731b09590263aeae8029e14529bb96f219c6678
ivegasgQ103 <i.vegasg@alumnos.upm.es>
2020-03-25 19:34:30 +0100

    Update TRABAJO.c
    He desarrollado el menu. He incluido las opciones.

--- TRABAJO.c
+++ TRABAJO.c
@@ -1,13 +1,42 @@
-//TRABAJO CALCULADORA CIENTIFICA 2019-2020
-
-
 #include<stdio.h>
-#include<math.h>
 #include<string.h>
-int main(){
-	
+#include<math.io>
 
-
+//Menu del trabajo
+void main()
+{
+	char ;
+	int option=0;
 	
-	return 0;
+	do
+	{	
+		printf("Elija una opcion: 0-Apagar Sistema\n,1-Operaciones Basicas\n,2-Operaciones con Vectores\n,3-Constantes numericas\n,4-Tabla Periodica\n,5-Sistemas de ecuaciones\n,6-Operaciones complejas\n\n");
+		scanf("%d",&option);
+	if((option<0)||(option>7))
+		{
+			printf("Error 404 Not Found");
+		}
+	else
+		{
+			switch(option)
+			{
+				case 0: printf("Apagando calculadora.\n");
+				break;
+			
+				case 1: printf("Operaciones Basicas\n");
+				break;
+				case 2: printf("Operaciones con Vectores\n");
+				break;
+				case 3: printf("Constantes numericas\n");
+				break;
+				case 4: printf("Tabla Periodica\n");
+				break;
+				case 5: printf("Sistemas de ecuaciones\n");
+				break;
+				case 6: printf("Operaciones complejas\n");
+				break;
+			}
+		}
+	}
+	while(option!=0);
 }
