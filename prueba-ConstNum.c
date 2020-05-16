3511cc79cb3efb65e95a5ee9a8f2f806578b4621
ivegasgQ103 <i.vegasg@alumnos.upm.es>
2020-05-16 13:34:15 +0200

    Ya esta incluida la opcion de repetir

--- prueba-ConstNum.c
+++ prueba-ConstNum.c
@@ -17,70 +17,77 @@ int ConstantesNum()
 	int i,j;
 	FILE *pfentrada;
 	struct TConstante tabla[200];
-	int option,option2,N=0;
+	int option,option2,option3,N=0;
 	
 	
 		printf("Has elegido constantes numericas.\n");
 		printf("\n Quieres salir de esta opcion? 1.-Si\t 2.-No\n");
 		scanf("%d",&option2);
 
-
-		if(option2==2)
-		{
-			do
-			{	
-				printf("Que desea hacer?\n 1.-Registrar Constantes \n 2.-Mostrar tabla\n");
-				scanf("%d",&option);
-	
-				if(option==1)
-				{    		
-					printf("Cuantas constantes quiere registrar?\n");
-					scanf("%d",&N);
-					pfentrada=fopen("tabla2.txt","a");	
-					//pido los datos al usuario sobre los elementos
-
-					for(i=0;i<N;i++)
-					{
-						printf("Introduzca los datos de la constante:\n \n");
-						printf("Introduzca el nombre: \t");
-						scanf("%s", tabla[i].nombre);
-						fprintf(pfentrada,"%s\t", tabla[i].nombre);	
-						fflush(stdin);
-					
-						printf("Introduzca el numero: \t");
-						scanf("%s", tabla[i].numero);	
-						fprintf(pfentrada,"\t  %s ", tabla[i].numero);
-						fflush(stdin);
-				}			
-
-			fclose(pfentrada);
-			}
-			else if(option==2)
+		
+			if(option2==2)
 			{
-					pfentrada=fopen("tabla2.txt","r");
+				do
+				{
+					printf("Que desea hacer?\n 1.-Registrar Constantes \n 2.-Mostrar tabla\n");
+					scanf("%d",&option);
 	
-					if(pfentrada== NULL)
-					{
-						printf("No se ha encontrado un fichero con ese nombre.\n");
-						return 0;
-					}			
+					if(option==1)
+					{    	
 					
-					j=0;
-					printf("Nombre \t\t numero \n");
-					while(fscanf(pfentrada,"%s %s", tabla[j].nombre, tabla[j].numero)!=EOF)
-					{
-						printf("%s\t\t %s\n", tabla[j].nombre,tabla[j].numero);
-						j++;
-						
+						printf("Cuantas constantes quiere registrar?\n");
+						scanf("%d",&N);
+						pfentrada=fopen("tabla2.txt","a");	
+
+
+						for(i=0;i<N;i++)
+						{
+							printf("Introduzca los datos de la constante:\n \n");
+							printf("Introduzca el nombre: \t");
+							scanf("%s", tabla[i].nombre);
+							fprintf(pfentrada,"%s\t", tabla[i].nombre);	
+							fflush(stdin);
+					
+							printf("Introduzca el numero: \t");
+							scanf("%s", tabla[i].numero);	
+							fprintf(pfentrada,"\t  %s ", tabla[i].numero);
+							fflush(stdin);
+						}			
+	
+						fclose(pfentrada);
+							
 					}
-					fclose(pfentrada);
-			}
+					
+					else if(option==2)
+					{
+						pfentrada=fopen("tabla2.txt","r");
+						
+						if(pfentrada== NULL)
+						{
+							printf("No se ha encontrado un fichero con ese nombre.\n");
+							return 0;
+						}			
+					
+						j=0;
+						printf("Nombre \t\t numero \n");
+						while(fscanf(pfentrada,"%s %s", tabla[j].nombre, tabla[j].numero)!=EOF)
+						{
+							printf("%s\t\t %s\n", tabla[j].nombre,tabla[j].numero);
+							j++;
+						
+						}
+						fclose(pfentrada);
+					}
+					printf("Quiere volver a realizar la operacion?\n\t 1.-Si\n\t 2.-No\n\n\t");
+					scanf("%d",&option3);
 			
+				}
+				while(option3==1);
+			}	
+		
+			else
+			{
+				printf("Apagando...");
 			}
-			while(option2<2|| option>1);
-	}
-	else
-	{
-		printf("Apagando...");
-	}
 }
+
